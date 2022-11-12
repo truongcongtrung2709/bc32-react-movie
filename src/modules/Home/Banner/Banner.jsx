e90cdc3e734fcf4e732d17ab0f8261f6409e7688
trung <truongcongtrung2709@gmail.com>
2022-11-12 21:31:46 +0700

    done banner

--- src/modules/Home/Banner/Banner.jsx
+++ src/modules/Home/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import movieAPI from "../../../services/movieAPI";
 import styles from "./banner.module.scss";
+import { Carousel } from "react-bootstrap";
 
 const Banner = () => {
   const [banners, setBanners] = useState([]);
@@ -14,35 +15,18 @@ const Banner = () => {
   }, []);
 
   return (
-    <div className="background" style={{backgroundImage:`url("https://t4.ftcdn.net/jpg/04/51/76/13/360_F_451761307_8lo4yWKiWgiO5LCF4m8Y3Bns7aLCT9MR.jpg")`, backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover"}}>
-    <div className={styles.banners__container}>
-    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-  <ol className="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
-    <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-    <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-  </ol>
-  <div className="carousel-inner">
-  {banners.map((item, index)=>(
-    <div className={index===0 ? "carousel-item active": "carousel-item"} key={item.maBanner}>
-      <img className="d-block w-100" src={item.hinhAnh} alt={`banner-${item.maBanner}`}/>
-    </div>
+    <Carousel>
+      {banners.map((item)=>(
+    <Carousel.Item key={item.maBanner}>
+      <img
+        className={` d-block w-100  ${styles.banner__item}`}
+        src={item.hinhAnh}
+        alt={`banner${item.maBanner}`}
+        height="700px"
+      />
+    </Carousel.Item> 
     ))}
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true" />
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true" />
-    <span className="sr-only">Next</span>
-  </a>
-</div>
-
-
-        
-        </div>
-        </div>
+  </Carousel>
   );
 };
 
